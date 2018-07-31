51b7872c5cbbf4cd9275a381d101684ce791ecee
Vsaxena ubc <vsaxena.ubc@gmail.com>
2018-07-30 23:49:00 -0700

    HomeWork - 07-Social Analytics

---
+++ 07-Social-Analytics/config.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = "RP3D7X8fhzWlPoJaaQs6ChpPH"
+consumer_secret = "CKS437Lq1sTBMgBFGXVqps1XYsy1q3Vwq5RfhNXhOAHEaSfbYS"
+access_token = "1019760221487415296-WJf0sJcRQzS0CfBdr7NHhLcrdh455U"
+access_token_secret = "xI8Gq12sIAdLGE8sM3kAV136cxI1OVgHY6n2b6iMwKh2S"
