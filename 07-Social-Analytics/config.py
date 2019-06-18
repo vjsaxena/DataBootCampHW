d68a2d103c0730d49e270f1cc11e9c59c83f2a8f
vjsaxena <vsaxena.ubc@gmail.com>
2019-06-17 23:38:10 -0700

    Update config.py

--- 07-Social-Analytics/config.py
+++ 07-Social-Analytics/config.py
@@ -1,5 +1,5 @@
 # Twitter API Keys
-consumer_key = "RP3D7X8fhzWlPoJaaQs6ChpPH"
-consumer_secret = "CKS437Lq1sTBMgBFGXVqps1XYsy1q3Vwq5RfhNXhOAHEaSfbYS"
-access_token = "1019760221487415296-WJf0sJcRQzS0CfBdr7NHhLcrdh455U"
-access_token_secret = "xI8Gq12sIAdLGE8sM3kAV136cxI1OVgHY6n2b6iMwKh2S"
+consumer_key = "xxxx"
+consumer_secret = "xxx"
+access_token = "xxxx"
+access_token_secret = "xxxx"
